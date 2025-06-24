67ed036f6c5fc4297e8ee62f802ddd96e6d5042e
aroliani <148553795+aroliani@users.noreply.github.com>
2025-06-24 15:18:07 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import daunImg from '../assets/daun.jpg';
 const Home = () => {
   return (
     <div className="home-container">
-      <h1 className="home-title">Welcome to the Home Page</h1>
+      <h1 className="home-title">Welcome to the Home Page!</h1>
       <img
         src={daunImg}
         alt="daun.jpg"
