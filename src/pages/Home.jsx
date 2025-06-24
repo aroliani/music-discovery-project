8067193d0dbe2fbe1d142ba872ee45396583cdf9
aroliani <148553795+aroliani@users.noreply.github.com>
2025-06-24 15:17:13 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,14 @@
+import daunImg from '../assets/daun.jpg'; 
+
 const Home = () => {
   return (
     <div className="home-container">
       <h1 className="home-title">Welcome to the Home Page</h1>
+      <img
+        src={daunImg}
+        alt="daun.jpg"
+        style={{ width: 180, marginTop: 24 }}
+      />
     </div>
   );
 };
