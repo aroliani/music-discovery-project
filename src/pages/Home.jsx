6f5b8c15618a76f7fd2c66d2ce30a3c898470dfd
aroliani <148553795+aroliani@users.noreply.github.com>
2025-06-24 14:47:56 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
-const Home = ()=> {
-    return(
-        <div>
-            Home page
-        </div>
-    )
-}
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1 className="home-title">Welcome to the Home Page</h1>
+    </div>
+  );
+};
 
 export default Home;
